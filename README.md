d6165ffb2419149db2618097943e3ac3e40f136c
Ketan <coderguy.ketankauntia@gmail.com>
2022-03-05 00:19:54 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Forkify
+
+- Uses API to render data upon searching for recipe's of a specific food.
+- Display's data from the API such as ingredients, servings, cooking-time.
+- Have not implemented the bookmark feature tho :|
+
+- Project from Jonas Schedman's Js course.
+
+- Take a quick view of the project below,
---
+++ src/js/controller.js
@@ -0,0 +1,82 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+// import { of } from 'core-js/core/array';
+
+// if (module.hot) {
+//   module.hot.accept();
+// }
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
+
+    if (!id) return;
+
+    recipeView.renderSpinner();
+
+    //0) Update results view to mark search
+    resultsView.update(model.getSearchResultsPage());
+
+    // 1) Loading recipe
+    await model.loadRecipe(id);
+
+    // 2) Rendering recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    recipeView.renderError();
+  }
+};
+
+const controlSearchResults = async function () {
+  try {
+    resultsView.renderSpinner();
+
+    // 1) Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    //2) load search resutls
+    await model.loadSearchResults(query);
+
+    //3) Render Results
+    // resultsView.render(model.state.search.results);
+    resultsView.render(model.getSearchResultsPage());
+
+    //4) Render the initial pagination buttons
+    paginationView.render(model.state.search);
+  } catch (err) {
+    console.log(err);
+  }
+};
+controlSearchResults();
+
+const controlPagination = function (goToPage) {
+  //3) Render New Pagination Results
+  resultsView.render(model.getSearchResultsPage(goToPage));
+
+  //4) Render new pagination buttons
+  paginationView.render(model.state.search);
+};
+
+const controlServings = function (newServings) {
+  //Update the recipe sevings
+  model.updateServings(newServings);
+
+  //update the recipe view
+  // recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
+};
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+  recipeView.addHandlerUpdateServings(controlServings);
+  searchView.addHandlerSearch(controlSearchResults);
+  paginationView._addHandlerCLick(controlPagination);
+};
+init();
